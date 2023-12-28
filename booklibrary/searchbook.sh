d384338ee5d5e3e25acebd84afb4fb9b956894f7
perna <a210037@students.gu.edu.lb>
2023-12-28 16:17:41 +0200

    intial commit

---
+++ booklibrary/searchbook.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# this code allows us to search in open libarary for books
+
+searchBook()
+{
+echo "please enter the book you want to search for : "
+read input
+output=$(curl -s "https://openlibrary.org/search.json?q=$input")
+echo "Here is what I found : "
+echo "$output"
+}
+searchBook
---
+++ booklibrary/sorted.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#thsi code allows us to search by category thus giving us sorted search
+
+sortedSearchBook()
+{
+echo "Please enter the book you want to search for : "
+read -r input
+echo "How do you like it to be sorted by (relevance, new, popular, rating, publish_date, num_editions):"
+read -r sort
+
+output=$(curl -s "https://openlibrary.org/search.json?q=$input&sort=$sort")
+echo "Here is what I found : "
+echo "$output"
+}
+sortedSearchBook
